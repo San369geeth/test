706f96b20e0a42578221aba5818f622ae648c661
San369geeth <95426877+San369geeth@users.noreply.github.com>
2024-09-01 14:05:28 +0530

    Update master.py

--- backend/master.py
+++ backend/master.py
@@ -25,31 +25,31 @@ def run_subprocess(command, progress_message):
     subprocess.run(command)
 
 def extract_audio(video_path, output_audio_path):
-    thread = threading.Thread(target=run_subprocess, args=(['python', 'C:/Users/skfog/OneDrive/Desktop/project/backend/extract_audio.py', video_path, output_audio_path], "Extracting audio..."))
+    thread = threading.Thread(target=run_subprocess, args=(['python', '/backend/extract_audio.py', video_path, output_audio_path], "Extracting audio..."))
     thread.start()
     thread.join()  # Wait for the thread to complete
     send_progress("Audio extracted successfully.")
 
 def audio_to_text(audio_path, output_text_path):
-    thread = threading.Thread(target=run_subprocess, args=(['python', 'C:/Users/skfog/OneDrive/Desktop/project/backend/audio_to_text.py', audio_path, output_text_path], "Converting audio to text..."))
+    thread = threading.Thread(target=run_subprocess, args=(['python', '/backend/audio_to_text.py', audio_path, output_text_path], "Converting audio to text..."))
     thread.start()
     thread.join()
     send_progress("Audio converted to text successfully.")
 
 def translate_text(input_text_path, output_translated_text_path, target_language):
-    thread = threading.Thread(target=run_subprocess, args=(['python', 'C:/Users/skfog/OneDrive/Desktop/project/backend/translate_text.py', input_text_path, output_translated_text_path, target_language], "Translating text..."))
+    thread = threading.Thread(target=run_subprocess, args=(['python', '/backend/translate_text.py', input_text_path, output_translated_text_path, target_language], "Translating text..."))
     thread.start()
     thread.join()
     send_progress("Text translated successfully.")
 
 def text_to_speech(input_text_path, output_audio_path, language_code):
-    thread = threading.Thread(target=run_subprocess, args=(['python', 'C:/Users/skfog/OneDrive/Desktop/project/backend/text_to_speech.py', input_text_path, output_audio_path, language_code], "Converting text to speech..."))
+    thread = threading.Thread(target=run_subprocess, args=(['python', '/backend/text_to_speech.py', input_text_path, output_audio_path, language_code], "Converting text to speech..."))
     thread.start()
     thread.join()
     send_progress("Text converted to speech successfully.")
 
 def merge_audio_to_video(input_video_path, translated_audio_path, output_video_path):
-    thread = threading.Thread(target=run_subprocess, args=(['python', 'C:/Users/skfog/OneDrive/Desktop/project/backend/lastout.py', input_video_path, translated_audio_path, output_video_path], "Merging audio with video..."))
+    thread = threading.Thread(target=run_subprocess, args=(['python', '/backend/lastout.py', input_video_path, translated_audio_path, output_video_path], "Merging audio with video..."))
     thread.start()
     thread.join()
     send_progress("Audio merged with video successfully.")
@@ -59,11 +59,11 @@ if __name__ == "__main__":
     lang = sys.argv[2]
     socket_id = sys.argv[3]
     
-    audio_path = 'C:/Users/skfog/OneDrive/Desktop/project/site/output/og_audio.wav'
-    text_path = "C:/Users/skfog/OneDrive/Desktop/project/site/output/output_text.txt"
-    translated_text_path = "C:/Users/skfog/OneDrive/Desktop/project/site/output/output_translated_text.txt"
-    translated_audio_path = "C:/Users/skfog/OneDrive/Desktop/project/site/output/output_translated_audio.wav"
-    final_video_path = "C:/Users/skfog/OneDrive/Desktop/project/site/output/output_video.mp4"
+    audio_path = 'gs://culturacast-12b35.appspot.com/videotoaudio/og_audio.wav'
+    text_path = "gs://culturacast-12b35.appspot.com/audiototext/output_text.txt"
+    translated_text_path = "gs://culturacast-12b35.appspot.com/texttranslate/output_translated_text.txt"
+    translated_audio_path = "gs://culturacast-12b35.appspot.com/translatetexttoaudio/output_translated_audio.wav"
+    final_video_path = "gs://culturacast-12b35.appspot.com/opvideo/output_video.mp4"
     
 
     extract_audio(video_path, audio_path)
